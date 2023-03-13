030168edcf70ebc63e9fbf33a7d5dc784afbc6ab
Hema Kar <mala.sulekha@gmail.com>
2023-03-13 21:50:07 +0000

    instrument honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,6 +33,13 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+#Honeycomb------------
+# Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
